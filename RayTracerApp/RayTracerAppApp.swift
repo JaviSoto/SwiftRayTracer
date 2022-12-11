49e75aa34708ff3a7737432c7dce7a78018e7f14
Javier Soto <ios@javisoto.es>
2022-12-11 11:51:40 -0800

    Initial Commit

---
+++ RayTracerApp/RayTracerAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  RayTracerAppApp.swift
+//  RayTracerApp
+//
+//  Created by Javier Soto on 12/11/22.
+//
+
+import SwiftUI
+
+@main
+struct RayTracerAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
